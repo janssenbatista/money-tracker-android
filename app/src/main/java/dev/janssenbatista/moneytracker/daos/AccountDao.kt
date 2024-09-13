4a3cf4006d69d6cb4ee23e8928f558bde9cc001e
Janssen Batista <batistajanssen.dev@gmail.com>
2024-09-13 19:37:39 -0300

    feat: create account dao

---
+++ app/src/main/java/dev/janssenbatista/moneytracker/daos/AccountDao.kt
@@ -0,0 +1,20 @@
+package dev.janssenbatista.moneytracker.daos
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import dev.janssenbatista.moneytracker.models.Account
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AccountDao {
+
+    @Upsert
+    fun addAccount(account: Account)
+
+    @Query("SELECT * FROM tb_accounts")
+    fun getAllAccounts(): Flow<List<Account>>
+
+    @Query("SELECT * FROM tb_accounts WHERE id = :accountId")
+    fun getAccountById(accountId: Int): Flow<Account?>
+}
