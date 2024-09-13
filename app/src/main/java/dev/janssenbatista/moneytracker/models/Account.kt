99a77573d7a127082bafab939e8f3d526d3690ff
Janssen Batista <batistajanssen.dev@gmail.com>
2024-09-13 19:33:34 -0300

    feat: create account model

---
+++ app/src/main/java/dev/janssenbatista/moneytracker/models/Account.kt
@@ -0,0 +1,19 @@
+package dev.janssenbatista.moneytracker.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity(tableName = "tb_accounts")
+data class Account(
+    @PrimaryKey
+    val id: Int,
+    val description: String,
+    val amount: BigDecimal,
+    @ColumnInfo("created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @ColumnInfo("updated_at")
+    val updatedAt: LocalDateTime
+)
