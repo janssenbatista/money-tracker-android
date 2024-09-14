35be2fd06ea1c0282bd6c2a3d1f3796b582ea1ee
Janssen Batista <batistajanssen.dev@gmail.com>
2024-09-14 15:30:43 -0300

    feat: add account component

---
+++ app/src/main/java/dev/janssenbatista/moneytracker/components/AccountComponent.kt
@@ -0,0 +1,69 @@
+package dev.janssenbatista.moneytracker.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.janssenbatista.moneytracker.R
+import dev.janssenbatista.moneytracker.models.Account
+import dev.janssenbatista.moneytracker.models.AccountType
+import java.text.NumberFormat
+
+@Composable
+fun AccountComponent(account: Account, numberFormat: NumberFormat) {
+    Card(
+        Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            Modifier.padding(vertical = 8.dp, horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Box(
+                Modifier
+                    .size(36.dp)
+                    .clip(RoundedCornerShape(50))
+                    .background(color = Color.White),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = if (account.accountType == AccountType.WALLET.id)
+                            R.drawable.ic_wallet else
+                            R.drawable.ic_account_bank
+                    ),
+                    contentDescription = null
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = account.description,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium
+                )
+                Text(text = numberFormat.format(account.amount))
+            }
+        }
+    }
+}
